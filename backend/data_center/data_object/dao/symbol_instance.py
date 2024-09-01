ae9efec51b9f6e334d462eefa3e8dae5d9c0374d
Rain1047 <3487851868@qq.com>
2024-09-01 16:43:26 +0800

    添加历史数据
    Signed-off-by: Rain1047 <3487851868@qq.com>

---
+++ backend/data_center/data_object/dao/symbol_instance.py
@@ -0,0 +1,40 @@
+# 创建基类
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import declarative_base
+
+from backend.service.utils import DatabaseUtils
+
+Base = declarative_base()
+
+
+# 定义 ORM 模型类
+class SymbolInstance(Base):
+    __tablename__ = 'symbol_instance'
+    id = Column(Integer, primary_key=True, autoincrement=True, comment='交易对实例ID')
+    symbol = Column(String(255), nullable=False, comment='交易对')
+    interval = Column(String(255), nullable=False, comment='时间窗口')
+
+    # 查询方法
+
+
+def query_symbol_instance(symbol, interval) -> SymbolInstance:
+    session = DatabaseUtils.get_db_session()
+    return session.query(SymbolInstance).filter_by(symbol=symbol, interval=interval).first()
+
+    # 添加方法
+
+
+def add_symbol_instance(symbol, interval):
+    session = DatabaseUtils.get_db_session()
+    new_symbol_instance = SymbolInstance(symbol=symbol, interval=interval)
+    session.add(new_symbol_instance)
+    session.commit()
+
+
+if __name__ == '__main__':
+    # 添加一个新的交易对实例
+    add_symbol_instance('ETH', '4H')
+
+    # 查询交易对实例
+    result = query_symbol_instance('BTC', '4H')
+    print(result.symbol, result.interval)
---
+++ backend/data_center/kline_data/kline_data_collector.py
@@ -0,0 +1,46 @@
+import os
+
+import pandas as pd
+from tvDatafeed import TvDatafeed, Interval
+
+from backend.service.utils import ConfigUtils
+
+
+class KlineDataCollector:
+
+    def __init__(self):
+        self._load_config()
+        self.tv = TvDatafeed(self.config['tradingview_account'],
+                             self.config['tradingview_password'])
+
+    def _load_config(self):
+        self.config = ConfigUtils.get_config()
+
+    def collect_data(self, ticker: str, interval: Interval):
+        df = self.tv.get_hist(symbol=ticker + "USDT",
+                              exchange='Binance',
+                              interval=interval,
+                              n_bars=5000)
+        # 定义CSV文件名
+        filename = f'{ticker}-{interval.value}.csv'
+        if os.path.exists(filename):
+            # 如果文件存在，读取已有数据
+            existing_data = pd.read_csv(filename, index_col='datetime', parse_dates=True)
+
+            # 合并新数据与已有数据，并去除重复
+            combined_data = pd.concat([existing_data, df])
+            combined_data = combined_data[~combined_data.index.duplicated(keep='last')]
+
+            # 按时间顺序排序
+            combined_data.sort_index(inplace=True)
+
+            # 保存合并后的数据
+            combined_data.to_csv(filename, index=True)
+        else:
+            # 如果文件不存在，直接保存数据（包括表头）
+            df.to_csv(filename, mode='a', index=True)
+
+
+if __name__ == '__main__':
+    tv = KlineDataCollector()
+    tv.collect_data('SOL', Interval.in_daily)
