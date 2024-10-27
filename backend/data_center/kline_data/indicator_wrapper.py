9be477bfc288ddf77dd49bc14fec42e51f020b3a
Rain1047 <3487851868@qq.com>
2024-10-27 11:52:32 +0800

    准备开发回测系统 need backtest system badly
    Signed-off-by: Rain1047 <3487851868@qq.com>

--- backend/data_center/kline_data/indicator_wrapper.py
+++ backend/data_center/kline_data/indicator_wrapper.py
@@ -1,19 +1,8 @@
 import talib as ta
-import pandas as pd
 from pandas import DataFrame
-from backend.data_center.kline_data.kline_data_collector import *
 
 
 class IndicatorWrapper:
-    """
-    A class to wrap around a DataFrame and add various technical indicators.
-    """
-
-    def __init__(self):
-        """
-        Initialize the IndicatorWrapper without a DataFrame.
-        """
-        pass
 
     @staticmethod
     def add_indicator(df: DataFrame) -> DataFrame:
@@ -27,22 +16,15 @@ class IndicatorWrapper:
             raise ValueError("DataFrame must contain 'high', 'low', and 'close' columns.")
 
         # ADX Indicator
-        df['adx'] = ta.ADX(df['high'].values, df['low'].values, df['close'].values, timeperiod=14)
+        df['adx'] = ta.ADX(df['high'].values, df['low'].values, df['close'].values, timeperiod=14).round(2)
 
         # SMA Indicator
         SMA_PERIODS = [10, 20, 50, 100, 200]
         for period in SMA_PERIODS:
-            df[f'sma{period}'] = ta.SMA(df['close'].values, timeperiod=period)
+            df[f'sma{period}'] = ta.SMA(df['close'].values, timeperiod=period).round(2)
 
         return df
 
 
-# Example usage:
-if __name__ == '__main__':
-    tv = KlineDataCollector()
-    file_abspath = tv.get_abspath(symbol='BTC', interval=Interval.in_daily)
-    df = pd.read_csv(f"{file_abspath}")
-    wrapper = IndicatorWrapper()
-    df_with_indicators = wrapper.add_indicator(df)
-    print(df_with_indicators.head())
+
 
