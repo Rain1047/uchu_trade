9491489ca52be6c121e2cd0b1b40805af3c16a5e
Rain1047 <3487851868@qq.com>
2024-11-11 23:14:26 +0800

    处理策略链路
    Signed-off-by: Rain1047 <3487851868@qq.com>

--- backend/data_center/data_object/dto/strategy_instance.py
+++ backend/data_center/data_object/dto/strategy_instance.py
@@ -9,34 +9,40 @@ from backend.data_center.data_object.enum_obj import EnumTradeEnv
 class StrategyInstance(BaseModel):
     id: Optional[int] = None
     # 交易对 eg.BTC-USDT
-    tradePair: str
-    # 每次下单数量，整数 eg.1000
-    # position: int = 0
+    trade_pair: str
     # 允许交易方向
-    side: str = "all"
+    side: str
     # 每笔交易损失
-    lossPerTrans: Optional[int] = 0
-    # 实例已成交的订单数量
-    positionCount: Optional[int] = 0
+    loss_per_trans: Optional[int] = 0
+    # 每次下单数量，整数 eg.1000
+    entry_per_trans: Optional[int] = 0
+
     # 时间窗口 eg.1D, 4H
-    timeFrame: str
-    # 订单信息
-    orderInfo: str = ""
-    # 买入价格
-    entryPrice: Optional[float] = -1.0
-    # 买入策略id
-    stEntryCode: str
-    # 卖出策略id
-    stExitCode: str
+    time_frame: str
+
+    # 买入策略code
+    entry_st_code: str
+    # 卖出策略code
+    exit_st_code: str
+    # 过滤策略code
+    filter_st_code: str
     # 策略实例开关
     switch: int = 1
     # 删除
     delete: int = 0
     # 实盘虚拟盘
     flag: Optional[str] = "0"
-    # 创建时间
-    gmtCreate: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    # 更新时间
-    gmtUpdate: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     # 是否实盘
     env: str = EnumTradeEnv.DEMO.value
+
+    # 实例已成交的订单数量
+    positionCount: Optional[int] = 0
+    # 订单信息
+    orderInfo: str = ""
+    # 买入价格
+    entryPrice: Optional[float] = -1.0
+    # 创建时间
+    gmtCreate: str = datetime.now()
+    # 更新时间
+    gmtUpdate: str = datetime.now()
+
