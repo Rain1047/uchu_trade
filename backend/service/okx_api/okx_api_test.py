c5ed9258f017f762c96236eac4d2fbf51feb3af2
Rain1047 <3487851868@qq.com>
2024-11-15 23:37:29 +0800

    处理策略链路
    Signed-off-by: Rain1047 <3487851868@qq.com>

--- backend/service/okx_api/okx_api_test.py
+++ backend/service/okx_api/okx_api_test.py
@@ -1,7 +1,9 @@
+from backend.data_center.data_object.dao.account_balance import AccountBalance
 from backend.service.okx_api.okx_main_api import OKXAPIWrapper
 
 if __name__ == '__main__':
     okx = OKXAPIWrapper()
     account = okx.account
-    print(account.get_account_balance())
-
+    response = account.get_account_balance()
+    # AccountBalance.insert_or_update(response)
+    print(len(AccountBalance.list_all()))
