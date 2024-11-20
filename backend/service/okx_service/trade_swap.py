d54bbaa4545d9edc0c10530f1a0bcc1fd9e22212
Rain1047 <3487851868@qq.com>
2024-11-20 09:45:46 +0800

    调度链路
    Signed-off-by: Rain1047 <3487851868@qq.com>

--- backend/service/okx_service/trade_swap.py
+++ backend/service/okx_service/trade_swap.py
@@ -11,21 +11,33 @@ trade = okx.trade_api
 account = okx.account
 
 
-def get_swap_order(instId=''):
-    print(trade.get_order(instId=instId))
+# 取消永续合约的委托
+def cancel_swap_unfinished_algo_order(swap_unfinished_algo_list):
+    cancel_algo_list = []
+    for algo_order in swap_unfinished_algo_list:
+        cancel_algo_list.append(
+            {
+                'instId': algo_order.get('instId'),
+                'algoId': algo_order.get('algoId')
+            }
+        )
+    trade.cancel_algo_order(cancel_algo_list)
 
 
-def get_swap_order_list(instType):
-    print(trade.get_order_list(instType=instType))
+# 获取合约未完成的限价止盈委托
+def get_swap_limit_order_list():
+    print(trade.get_order_list(
+        instType='SWAP', ordType='limit'))
 
 
-# 查看持仓信息
+# 查看永续合约持仓信息
 def list_swap_positions():
     result = account.get_positions(instType='SWAP')
     print(result)
 
 
-def list_spot_unfinished_algo_order() -> List[Dict[str, Any]]:
+# 获取合约未完成的止盈止损委托
+def list_swap_unfinished_algo_order() -> List[Dict[str, Any]]:
     """
     获取未完成的现货算法订单列表
 
@@ -34,8 +46,8 @@ def list_spot_unfinished_algo_order() -> List[Dict[str, Any]]:
     """
     try:
         result = trade.order_algos_list(
-            # instType='SWAP',
-            # ordType=EnumAlgoOrdType.CONDITIONAL.value
+            instType='SWAP',
+            ordType=EnumAlgoOrdType.CONDITIONAL_OCO.value
         )
 
         # 检查result和data是否存在且有效
@@ -52,7 +64,10 @@ def list_spot_unfinished_algo_order() -> List[Dict[str, Any]]:
 
 
 if __name__ == '__main__':
-    # result = list_spot_unfinished_algo_order()
-    # print(result)
-    # list_swap_positions()
-    get_swap_order_list(instType='SWAP')
+    # 永续合约持仓信息
+    list_swap_positions()
+    # 未完成的永续合约止盈止损委托
+    result = list_swap_unfinished_algo_order()
+    print(result)
+    # 永续合约的限价委托
+    get_swap_limit_order_list()
