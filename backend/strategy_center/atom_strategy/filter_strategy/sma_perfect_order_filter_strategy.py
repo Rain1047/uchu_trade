197a2ad13292eceb3e9785f22468834235f70c31
Rain1047 <3487851868@qq.com>
2024-12-14 10:18:38 +0800

    交易调度系统的完整链路开发
    Signed-off-by: Rain1047 <3487851868@qq.com>

--- backend/strategy_center/atom_strategy/filter_strategy/sma_perfect_order_filter_strategy.py
+++ backend/strategy_center/atom_strategy/filter_strategy/sma_perfect_order_filter_strategy.py
@@ -1,5 +1,8 @@
+from typing import Optional
+
 from pandas import DataFrame
 
+from backend.data_center.data_object.dto.strategy_instance import StrategyInstance
 from backend.strategy_center.atom_strategy.strategy_registry import registry
 
 
@@ -11,16 +14,15 @@ def sma_perfect_order_filter_strategy(df: DataFrame, stIns: Optional[StrategyIns
         return sma_perfect_order_filter_strategy_live(df)
 
 
-def sma_perfect_order_filter_strategy_live(df) -> bool:
+def sma_perfect_order_filter_strategy_live(df: DataFrame) -> bool:
     if not df.empty:
-        buy_mask = (df['sma10'] > df['sma20']) & (df['sma20'] > df['sma50'])
-        return buy_mask.iloc[-1]
+        last_row = df.iloc[-1]
+        return (last_row['sma10'] > last_row['sma20']) and (last_row['sma20'] > last_row['sma50'])
     return False
+
 
 def sma_perfect_order_filter_strategy_backtest(df: DataFrame) -> DataFrame:
     if not df.empty:
         buy_mask = (df['sma10'] > df['sma20']) & (df['sma20'] > df['sma50'])
         df.loc[~buy_mask, 'entry_sig'] = 0
     return df
-
-
