d2e798fe5de5b61f58754db718558dab1f0069d8
Rain1047 <3487851868@qq.com>
2024-12-15 00:19:34 +0800

    交易调度系统的完整链路开发
    Signed-off-by: Rain1047 <3487851868@qq.com>

--- backend/_constants.py
+++ backend/_constants.py
@@ -1,17 +1,12 @@
 # This file contains all the OKX error codes and success code.
 # TODO 后续可以用于重试
 
-SUCCESS_CODE = '0'
-# OKX ERROR CODES
-ORDER_NOT_EXIST = '51603'
-IP_NOT_INCLUDED = '50110'
-
-MARKET_REST = "https://www.okx.com"
-AWS_REST = "https://aws.okx.com"
-
 
 class okx_constants:
     SUCCESS_CODE = '0'
     # OKX ERROR CODES
     ORDER_NOT_EXIST = '51603'
     IP_NOT_INCLUDED = '50110'
+
+    MARKET_REST = "https://www.okx.com"
+    AWS_REST = "https://aws.okx.com"
