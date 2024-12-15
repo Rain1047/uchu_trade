1af0ffec7c5193629f34ad91641a240d6a8494f9
Rain1047 <3487851868@qq.com>
2024-12-15 14:37:23 +0800

    交易调度系统的完整链路开发
    Signed-off-by: Rain1047 <3487851868@qq.com>

--- backend/strategy_center/strategy_processor/strategy_modifier.py
+++ backend/strategy_center/strategy_processor/strategy_modifier.py
@@ -2,12 +2,17 @@ import logging
 from datetime import datetime
 from typing import Optional
 
+import pandas as pd
+from pandas import DataFrame
+
 from backend._utils import DatabaseUtils
 from backend.api_center.okx_api.okx_main_api import OKXAPIWrapper
 from backend.data_center.kline_data.kline_data_collector import KlineDataCollector
 from backend.object_center._object_dao.algo_order_record import AlgoOrderRecord
-from backend.object_center.enum_obj import EnumTradeEnv, EnumState
+from backend.object_center._object_dao.st_instance import StrategyInstance
+from backend.object_center.enum_obj import EnumTradeEnv, EnumState, EnumTimeFrame
 from backend.service_center.okx_service.trade_swap import TradeSwapManager
+from backend.strategy_center.atom_strategy.strategy_registry import registry
 
 
 class StrategyModifier:
@@ -26,12 +31,26 @@ class StrategyModifier:
         algo_order_record_list = AlgoOrderRecord.list_by_state(state=EnumState.LIVE.value)
         for algo_order_record in algo_order_record_list:
             print("StrategyModifier@main_task, processing algo order record: {}".format(algo_order_record))
-            # 1. 通过ord_id 查询订单状态
+            # 通过ord_id 查询订单状态
             # 如果仍然为 live，
             get_order_result = self.trade_api.get_order(instId=algo_order_record.symbol, ordId=algo_order_record.ord_id)
             order_state = get_order_result['data'][0]['state']
             if order_state == EnumState.LIVE.value:
-                print("StrategyModifier@main_task, order is still live, do nothing.")
+                print("StrategyModifier@main_task, order is still live.")
+                # 根据st_inst_id获取止损的策略code
+                st_inst = StrategyInstance.get_st_instance_by_id(id=algo_order_record.st_inst_id)
+                df = self._get_data_frame(st_inst)
+                exit_st_code = st_inst.exit_st_code
+                exit_strategy = registry.get_strategy(exit_st_code)
+                exit_result = exit_strategy(df, st_inst)
+
+                # amend_algo_order_result = self.trade_swap_manager.amend_algo_order(
+                #     instId=algo_order_record.symbol,
+                #     algoId=algo_order_record.attach_algo_cl_ord_id,
+                #     newSz=algo_order_record.fill_sz,
+                #     newPx=algo_order_record.fill_px
+                # )
+
             if order_state == EnumState.FILLED.value:
                 print("StrategyModifier@main_task, order is filled.")
                 # 订单结果参数，调用get_order方法
@@ -50,6 +69,15 @@ class StrategyModifier:
                 algo_order_record.update_time = datetime.now()
                 AlgoOrderRecord.save_or_update_algo_order_record(algo_order_record.to_dict())
                 print(get_order_data)
+
+    def _get_data_frame(self, st_instance) -> DataFrame:
+        # 获取df
+        symbol = st_instance.trade_pair.split('-')[0]
+        interval = EnumTimeFrame.get_enum_by_value(st_instance.time_frame)
+        file_abspath = self.data_collector.get_abspath(symbol=symbol, interval=interval)
+        print(f"StrategyExecutor@_get_data_frame, target file path: {file_abspath}")
+        df = pd.read_csv(f"{file_abspath}")
+        return df
 
 
 def _setup_logging():
