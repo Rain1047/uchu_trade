d6954a4db480ff69bc9a52e7d67608b80ee60307
Rain1047 <3487851868@qq.com>
2024-12-15 13:35:13 +0800

    交易调度系统的完整链路开发
    Signed-off-by: Rain1047 <3487851868@qq.com>

--- backend/strategy_center/strategy_processor/strategy_modifier.py
+++ backend/strategy_center/strategy_processor/strategy_modifier.py
@@ -4,11 +4,12 @@ from typing import Optional
 from backend._utils import DatabaseUtils
 from backend.api_center.okx_api.okx_main_api import OKXAPIWrapper
 from backend.data_center.kline_data.kline_data_collector import KlineDataCollector
-from backend.object_center.enum_obj import EnumTradeEnv
+from backend.object_center._object_dao.algo_order_record import AlgoOrderRecord
+from backend.object_center.enum_obj import EnumTradeEnv, EnumState
 from backend.service_center.okx_service.trade_swap import TradeSwapManager
 
 
-class StrategyExecutor:
+class StrategyModifier:
     def __init__(self, env: str = EnumTradeEnv.MARKET.value, time_frame: Optional[str] = None):
         self.env = env
         self.tf = time_frame
@@ -19,9 +20,20 @@ class StrategyExecutor:
         self.session = DatabaseUtils.get_db_session()
         _setup_logging()
 
+    def main_task(self):
+        print("StrategyModifier@main_task, starting strategy modifier.")
+        algo_order_record_list = AlgoOrderRecord.list_by_state(state=EnumState.LIVE.value)
+        for algo_order_record in algo_order_record_list:
+            print("StrategyModifier@main_task, processing algo order record: {}".format(algo_order_record))
+
 
 def _setup_logging():
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s'
     )
+
+
+if __name__ == '__main__':
+    strategy_modifier = StrategyModifier()
+    strategy_modifier.main_task()
