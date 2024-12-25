e5da5428e1dd4f4633b945537d22109041941f18
Rain1047 <3487851868@qq.com>
2024-12-26 00:05:32 +0800

    限价/止损委托全链路开发
    Signed-off-by: Rain1047 <3487851868@qq.com>

--- backend/schedule_center/tasks/trade_tasks/spot_stop_loss_task.py
+++ backend/schedule_center/tasks/trade_tasks/spot_stop_loss_task.py
@@ -163,8 +163,8 @@ class SpotStopLossTask:
                 latest_algo_order = self.trade.get_algo_order(algoId=manual_live_algo_order.get('algoId'))
                 if latest_algo_order and latest_algo_order.get('code') == '0':
                     if latest_algo_order.get('data')[0].get('state') != EnumAlgoOrderState.LIVE.value:
-                        SpotAlgoOrderRecord.update_status_by_ord_id(manual_live_algo_order.get('algoId'),
-                                                                    manual_live_algo_order.get('data')[0].get('state'))
+                        SpotAlgoOrderRecord.update_status_by_algo_id(manual_live_algo_order.get('algoId'),
+                                                                     manual_live_algo_order.get('data')[0].get('state'))
         # [调用接口] 获取所有未完成的订单
         algo_order_list_result = self.trade.order_algos_list(
             instType="SPOT", ordType="conditional,oco")
